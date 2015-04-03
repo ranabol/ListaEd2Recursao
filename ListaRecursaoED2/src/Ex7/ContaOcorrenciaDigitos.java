1cee9d455039e926f0ecea2c8cc029e0adb3f4a7
ranabol <raulz_ito@hotmail.com>
2015-04-03 18:59:10 -0300

    Exercicio 7

--- ListaRecursaoED2/src/Ex7/ContaOcorrenciaDigitos.java
+++ ListaRecursaoED2/src/Ex7/ContaOcorrenciaDigitos.java
@@ -1,7 +1,16 @@
 package Ex7;
 
 public class ContaOcorrenciaDigitos {
-	public int contaOcorrenciaDigitos(int n){
+	int vezes,dig;
+	public int contaOcorrenciaDigitos(int n,int k){
 		
+		dig = n%10;
+		n/=10;
+		if(dig==k)
+			vezes++;
+		if (n>=1){
+			contaOcorrenciaDigitos(n, k);
+		}
+		return vezes;
 	}
 }
--- ListaRecursaoED2/src/Ex7/TestaOcorrenciaDigitos.java
+++ ListaRecursaoED2/src/Ex7/TestaOcorrenciaDigitos.java
@@ -3,7 +3,10 @@ package Ex7;
 public class TestaOcorrenciaDigitos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 ContaOcorrenciaDigitos cd = new ContaOcorrenciaDigitos();
+		 cd.contaOcorrenciaDigitos(762021192, 2);
+		 System.out.println(cd.vezes);
+		 
 
 	}
 
